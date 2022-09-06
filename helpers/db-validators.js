4c446ba5272e0b264553b8ff7688dbe7174ab353
rdanielpolich <rdanielpolich@gmail.com>
2022-09-06 19:13:56 -0300

    primer commit con servidor base en el que se trabajo en clase, solo con usuarios

---
+++ helpers/db-validators.js
@@ -0,0 +1,24 @@
+const Usuario = require("../models/usuario");
+
+
+//VALIDAR EMAIL SI EXISTE
+const emailExiste = async (email) => {
+  const existeEmail = await Usuario.findOne({ email });
+  if (existeEmail) {
+    throw new Error(`El correo ${email} ya existe`);
+  }
+};
+
+//VALIDAR ID SI EXISTE
+const existeUsuarioPorId = async (id) => {
+  const existeUsuario = await Usuario.findOne({ _id: id });
+
+  if (!existeUsuario) {
+    throw new Error(`El id ${id} no existe`);
+  }
+};
+
+module.exports = {
+  emailExiste,
+  existeUsuarioPorId,
+};
